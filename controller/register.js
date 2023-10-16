9eecf102e204c21636aca8fb9448bf3c66de20ff
SiggiJr <fsiegert1@gmail.com>
2023-10-16 15:46:29 +0200

    Id Bug gefixed

--- controller/register.js
+++ controller/register.js
@@ -6,9 +6,9 @@ export const register = async (req, res) => {
   console.log(req.body)
   const db = await getDb()
   console.log(req.body)
-  // db.collection(COL).insertOne({ email: req.body.email, password: req.body.password })
+  await db.collection(COL).insertOne({ email: req.body.email, password: req.body.password })
   const user = await db.collection(COL).findOne({ email: req.body.email, password: req.body.password })
-  console.log(await user._id)
+  // console.log(user._id)
   res.json(JSON.stringify({ id: user._id }))
 }
 
