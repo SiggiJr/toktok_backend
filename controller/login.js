5cbf4abf2fca009e828726f7804c2d7cc3698542
SiggiJr <fsiegert1@gmail.com>
2023-10-17 15:11:45 +0200

    Login Route und encrypt erstellt

--- controller/login.js
+++ controller/login.js
@@ -1,1 +1,17 @@
-const jo = 1
+import { getDb } from '../utils/db.js'
+import { createToken } from '../utils/token.js'
+
+const COL = 'users'
+
+export const login = async (req, res) => {
+  const db = await getDb()
+  const userData = await db.collection(COL).findOne({ email: req.body.email, password: req.body.password })
+  res.cookie('token', createToken({ user: userData._id }), {
+    httpOnly: true,
+    sameSite: 'strict',
+    secure: true,
+    path: '/',
+  })
+  console.log(userData)
+  res.json(userData)
+}
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import { createProfile, register } from '../controller/register.js'
 import { checkToken } from '../middleware/authMiddleware.js'
+import { encrypt } from '../middleware/encrypt.js'
+import { login } from '../controller/login.js'
 
 export const router = new express.Router()
 
-router.post('/register', register)
+router.post('/register', encrypt, register)
 
 router.put('/createprofile', checkToken, createProfile)
+
+router.post('/login', encrypt, login)
