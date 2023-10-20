37f32b1605de80c397350c5c09d81051df441dca
SiggiJr <fsiegert1@gmail.com>
2023-10-20 11:16:19 +0200

    next push für Hector

--- routes/feedRoute.js
+++ routes/feedRoute.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import { getUserFeed } from '../controller/feedController.js'
+import { checkToken } from '../middleware/authMiddleware.js'
 
 export const router = new express.Router()
 
 // router.get('/feed', checkToken, getUserFeed)
-router.get('/getfeed', getUserFeed)
+router.get('/getfeed', checkToken, getUserFeed)
