05c5c01194557aaccb7cf9d0592a7574e5bd6e24
SiggiJr <fsiegert1@gmail.com>
2023-10-25 10:41:50 +0200

    Comment Id

--- controller/commentsController.js
+++ controller/commentsController.js
@@ -8,10 +8,12 @@ export const addComment = async (req, res) => {
   const comment = req.body
   const db = await getDb()
   const post = await db.collection('posts').findOne({ _id: new ObjectId(postId) })
+  console.log(post)
   comment.owner = userId
   comment.timestamp = new Date()
+  comment.comment_id = crypto.randomUUID
   post.comments.push(comment)
   await db.collection('posts').updateOne({ _id: new ObjectId(postId) }, { $set: { ...post } })
-  console.log(post)
+  // console.log(post)
   res.end()
 }
