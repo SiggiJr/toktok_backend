c6e0b9946432ce7ad33d8ce7d6e25b7dbf6dab35
SiggiJr <fsiegert1@gmail.com>
2023-10-26 18:38:13 +0200

    Update geht überall

--- routes/users.js
+++ routes/users.js
@@ -4,12 +4,15 @@ import { checkToken } from '../middleware/authMiddleware.js'
 import { encrypt } from '../middleware/encrypt.js'
 import { login, logout } from '../controller/login.js'
 import { getProfileData, getUserData } from '../controller/userController.js'
+import { updateProfile } from '../controller/updateController.js'
 
 export const router = new express.Router()
 
 router.post('/register', encrypt, register)
 
 router.put('/createprofile', checkToken, createProfile)
+
+router.put('/updateuser', checkToken, updateProfile)
 
 router.post('/login', encrypt, login)
 
